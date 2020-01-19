e720973bc19d715e41ad4828ea369bc7bf0f09a0
Cleveland Pitt Jr <clevelandpittjr@Clevelands-MacBook-Pro.local>
2020-01-19 01:27:52 -0500

    added to schema

--- employeeTrackerSchema.sql
+++ employeeTrackerSchema.sql
@@ -3,11 +3,27 @@ CREATE DATABASE employeeTracker_DB;
 
 USE employeeTracker_DB;
 
-CREATE TABLE auctions(
+CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT,
-  item_name VARCHAR(100) NOT NULL,
-  category VARCHAR(45) NOT NULL,
-  starting_bid INT default 0,
-  highest_bid INT default 0,
+  deparment_name VARCHAR(30),
   PRIMARY KEY (id)
 );
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT,
+ title VARCHAR(30),
+ salary DECIMAL,
+ department_id INT,
+  PRIMARY KEY (id)
+);
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM department;
+select * from role;
+select * from employee;
