a0cdf080f7e11f4cf8408464559f81deefa386b3
Cleveland Pitt Jr <clevelandpittjr@Clevelands-MacBook-Pro.local>
2020-01-21 15:05:20 -0500

    added console table and edited schema

--- employeeTrackerSchema.sql
+++ employeeTrackerSchema.sql
@@ -5,25 +5,25 @@ USE employeeTracker_DB;
 
 CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT,
-  deparment_name VARCHAR(30),
+  deparment_name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
-CREATE TABLE role(
+CREATE TABLE roles(
   id INT NOT NULL AUTO_INCREMENT,
- title VARCHAR(30),
- salary DECIMAL,
- department_id INT,
+  title VARCHAR(50) NOT NULL,
+  salary DECIMAL,
+  department_id INT NULL,
   PRIMARY KEY (id)
 );
 CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INT,
-  manager_id INT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
   PRIMARY KEY (id)
 );
 
 SELECT * FROM department;
-select * from role;
+select * from roles;
 select * from employee;
